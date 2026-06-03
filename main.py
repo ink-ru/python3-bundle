f75cfdd9f152a6b6ea1e236d4c3b4fc0918bccbd
Васин Юрий <ink2milk@gmail.com>
2026-06-03 12:15:56 +0300

    update

--- main.py
+++ main.py
@@ -2,44 +2,63 @@
 # -*- coding: utf-8 -*-
 
 '''
-Script description
+Script description: Empty python boilerplate skeleton wireframe template to start a new Python3 project from the scratch
+
+You can run this script by registering mimetype with binfmt_misc because shebang already provided
 '''
 
-import os, sys, re
+import os, sys
 import argparse
-import env_file
+from pathlib import Path
+from dotenv import load_dotenv
+
+dotenv_path = Path(__file__).parent / '.env'
+load_dotenv(dotenv_path)
 
 '''
 Loading constants:
-    VERSION
-    DEBUG
-    DB_NAME
-    DB_PASS
+DEBUG
+DB_NAME
+DB_PASS
 '''
-env_file.load()
+
+DEBUG = os.environ.get("DEBUG")
+DB_NAME = os.environ.get("DB_NAME") 
+DB_PASS = os.environ.get("DB_PASS")
+
+# Checking if the script is running as a subprocess
+parent_pid = os.getppid() if hasattr(os, 'getppid') else None
 
 def createParser (): # http://jenyay.net/Programming/Argparse
-	parser = argparse.ArgumentParser(
-				prog = sys.argv[0],
-				description = '''Pyhton3 script bundle''',
-				epilog = '''george.a.wise@gmail.com'''
-				)
-	parser.add_argument ('-v', '-verbose', required=False, default='0', help = 'Verbose output')
+    parser = argparse.ArgumentParser(
+                prog = sys.argv[0],
+                description = '''Pyhton3 script bundle''',
+                epilog = '''george.a.wise@gmail.com'''
+                )
+    parser.add_argument ('-v', '-verbose', nargs='?', const='1', default='0', help = 'Verbose output')
  
-	return parser
+    return parser
 
 def main(argv):
     parser = createParser()
-	namespace = parser.parse_args()
-    mode = namespace.v # sys.argv[1]
-    
-    pass
+    namespace = parser.parse_args()
+    mode = namespace.v  # sys.argv[1]
 
-	sys.exit(os.EX_OK) # code 0, all ok
+    print(f"verbose mode: {mode}")
+    
+
+    sys.exit(os.EX_OK) # code 0, all ok
 
 # assert __name__ is '__main__'
-if __name__ == '__main__':
-	# main(argv)
-	main(sys.argv)
+if __name__ == Path(__file__).stem:
+	print('Usage error, this is not a module!')
+	sys.exit(1)
+if not sys.stdin.isatty() or not sys.stdout.isatty() or (parent_pid and parent_pid == None):
+    print("Subprocess, no tty aviable!")
+    sys.exit(1)
+elif __name__ == '__main__':
+    main(sys.argv)
 else:
-	sys.exit(os.EX_USAGE) # https://docs.python.org/2/library/os.html
+    sys.exit(os.EX_USAGE) # https://docs.python.org/2/library/os.html
+
+
